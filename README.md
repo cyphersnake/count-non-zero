7bb7dc1fd1548122bb558b7a4d8a4ae49fee3642
cyphersnake <cyphersnake@pm.me>
2024-03-16 23:10:35 +0100

    chore: rename to `count_to_non_zero`

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-# CountNonZero
+# CountToNonZero
 
 ## Overview
-The CountNonZeroExt crate provides an extension trait for Rust's standard `Iterator` trait, enabling the counting of non-zero elements in iterators in a way that returns an `Option<NonZeroUsize>`. This approach optimally integrates with Rust's type system to offer a compile-time guarantee that the result, when present, is indeed non-zero. This functionality is particularly useful in scenarios where distinguishing between zero and non-zero counts is critical and can lead to more efficient code by leveraging the NonZeroUsize type's ability to optimize memory layouts and conditional logic.
+The CountToNonZeroExt crate provides an extension trait for Rust's standard `Iterator` trait, enabling the counting of non-zero elements in iterators in a way that returns an `Option<NonZeroUsize>`. This approach optimally integrates with Rust's type system to offer a compile-time guarantee that the result, when present, is indeed non-zero. This functionality is particularly useful in scenarios where distinguishing between zero and non-zero counts is critical and can lead to more efficient code by leveraging the NonZeroUsize type's ability to optimize memory layouts and conditional logic.
 
 ## Features
-Extends the `Iterator` trait with the `count_non_zero` method.
-The `count_non_zero` method counts the elements for which the iterator yields values, returning an `Option<NonZeroUsize>`.
+Extends the `Iterator` trait with the `count_to_non_zero` method.
+The `count_to_non_zero` method counts the elements for which the iterator yields values, returning an `Option<NonZeroUsize>`.
 
 ## Usage
 
 ```rust
 use std::num::NonZeroUsize;
 
-use count_non_zero_ext::CountNonZeroExt;
+use count_to_non_zero::CountToNonZeroExt;
 
 fn main() {
     let data = vec![1, 2, 0, 4];
-    let count = data.into_iter().filter(|el| el % 2 == 0).count_non_zero();
+    let count = data.into_iter().filter(|el| el % 2 == 0).count_to_non_zero();
     
     match count {
         Some(non_zero_count) => println!("Non-zero count of elements: {}", non_zero_count),
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
+#![doc = include_str!("../README.md")]
+
 use std::num::NonZeroUsize;
 
 /// An extension trait for iterators to count non-zero elements.
 ///
-/// This trait provides an additional method to iterators, `count_non_zero`,
+/// This trait provides an additional method to iterators, `count_to_non_zero`,
 /// which returns an `Option<NonZeroUsize>` representing the number of elements
 /// iterated over that are not zero. This method leverages Rust's type system
 /// to provide a guarantee at compile time that the count is non-zero when
 /// Some value is returned.
-pub trait CountNonZeroExt: Iterator {
+pub trait CountToNonZeroExt: Iterator {
     /// Counts the number of non-zero elements the iterator iterates over.
     ///
     /// # Returns
@@ -19,19 +21,19 @@ pub trait CountNonZeroExt: Iterator {
     /// # Examples
     ///
     /// ```
-    /// use count_non_zero::CountNonZeroExt;
+    /// use count_to_non_zero::CountToNonZeroExt;
     /// let data = vec![0, 1, 2, 0, 3];
-    /// let count = data.into_iter().count_non_zero();
+    /// let count = data.into_iter().count_to_non_zero();
     /// assert_eq!(count, Some(std::num::NonZeroUsize::new(5).unwrap()));
     /// ```
-    fn count_non_zero(self) -> Option<NonZeroUsize>;
+    fn count_to_non_zero(self) -> Option<NonZeroUsize>;
 }
 
-impl<T> CountNonZeroExt for T
+impl<T> CountToNonZeroExt for T
 where
     T: Iterator,
 {
-    fn count_non_zero(self) -> Option<NonZeroUsize> {
+    fn count_to_non_zero(self) -> Option<NonZeroUsize> {
         let count = self.count();
         NonZeroUsize::new(count)
     }
@@ -46,14 +48,14 @@ mod tests {
     #[test]
     fn empty_iterator() {
         let data: Vec<u32> = vec![];
-        let count = data.into_iter().count_non_zero();
+        let count = data.into_iter().count_to_non_zero();
         assert_eq!(count, None);
     }
 
     #[test]
     fn all_non_zeros_iterator() {
         let data = vec![1, 2, 3];
-        let count = data.into_iter().count_non_zero();
+        let count = data.into_iter().count_to_non_zero();
         assert_eq!(count, Some(NonZeroUsize::new(3).unwrap()));
     }
 }
